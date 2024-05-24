f685436225e47a06398db23ccdb96096dad6db4f
henryguajardo <henroki1989@gmail.com>
2024-05-23 23:41:13 -0400

    footer-agregado

--- index.php
+++ index.php
@@ -4,8 +4,86 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 <body>
-    <h1>hola</h1>
+     <!-- Footer -->
+  <footer class="text-center text-lg-start bg-footer">
+    <section class="d-flex justify-content-between p-4 bg-titfooter">
+      <div class="me-5">
+        <h3>Conéctate con nosotros:</h3>
+      </div>
+      <div>
+        <a href="" class="me-4" style="color: #ffffff">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4" style="color: #ffffff">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4" style="color: #ffffff">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4" style="color: #ffffff">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4" style="color: #ffffff">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4" style="color: #ffffff">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <section class="bg-footer">
+      <div class="container text-center text-md-start mt-5">
+        <div class="row mt-3">
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold">Energy and Water</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; height: 2px" />
+            <p>
+              Ofrecemos soluciones a medida para cada cliente, garantizando la calidad y
+              la seguridad de nuestros servicios. Contacta con nosotros y solicita tu presupuesto sin compromiso.
+            </p>
+          </div>
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold">Productos</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; height: 2px" />
+            <p>
+              <a href="#!" class="text-white">Aire acondicionado</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Sala de Calderas</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Grupo electrógeno</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Sala de bombas</a>
+            </p>
+          </div>
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Contactos</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; height: 2px" />
+            <p><i class="fas fa-home mr-3"></i> Manquehue Sur 520, oficina 205, Las Condes</p>
+            <p><i class="fas fa-envelope mr-3"></i> contacto@energyandwater.cl </p>
+            <p><i class="fas fa-phone mr-3"></i> +56 2 3256 9798 </p>
+            <p><i class="fas fa-print mr-3"></i> +56 9 3083 5203</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="text-center p-3">
+      © 2020 Copyright:
+      <a class="" href="www.energyandwater.cl">www.energyandwater.cl</a>
+    </div>
+
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
 </body>
 </html>
